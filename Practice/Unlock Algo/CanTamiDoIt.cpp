5a6eb1b8dc6e2b793da18f817ea34ff24a94de0e
its_rittik <rittikdas2017@gmail.com>
2024-03-01 23:16:11 +0600

    Unlock algo quesyion

---
+++ Practice/Unlock Algo/CanTamiDoIt.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n , p ,temp;
+    int return_day = 0 ;
+
+    cin>>n>>p;
+
+    vector<int> arr;
+
+    for (int i = 0 ; i < n ; i++) {
+        cin>>temp;
+        arr.push_back(temp);
+    }
+    sort(arr.begin() , arr.end());
+    int i = n ;
+    while(p>0 && i <= n){
+        if(p>=arr[i]) {
+            if (((p/arr[i])*arr[i]) > 0){
+                p -= ((p/arr[i])*arr[i]);
+                return_day++;
+            }
+        }
+        i--;
+    }
+    cout<<"Tami will be back after " <<return_day<< " days"<<endl;
+
+    return 0 ;
+}
