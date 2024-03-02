09eaf85c238b9ccfe762a49aa9f7ac6eaaa6f683
its_rittik <rittikdas2017@gmail.com>
2024-03-02 22:29:36 +0600

    Marge dort

---
+++ Sorting/marge_sort.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+void marge(vector<int>&arr , int low , int mid , int high){
+    vector<int>temp;
+    int left = low , right = mid+1 ;
+
+    while(left <= mid && right <= high){
+        if(arr[left]<=arr[right]) temp.push_back(arr[left++]);
+        else temp.push_back(arr[right++]);
+    }
+    
+    while(left <= mid) temp.push_back(arr[left++]);
+    while(right <= high) temp.push_back(arr[right++]);
+
+    for(int i = low ; i <= high ; i++) arr[i] = temp[i-low];
+}
+
+
+void marge_sort(vector<int>&arr , int low , int high){
+    int mid = (low+high)/2 ;
+    if(low >= high)
+        return;
+    marge_sort(arr , low, mid);
+    marge_sort(arr,mid+1, high);
+    marge(arr,low, mid, high);
+}
+
+int main(){
+    vector<int> arr = {1,4,2,5,6,3,9,8};
+    marge_sort(arr , 0 , arr.size()-1);
+    for(int i = 0 ; i < arr.size() ; i++) cout<<arr[i]<<" ";
+}
