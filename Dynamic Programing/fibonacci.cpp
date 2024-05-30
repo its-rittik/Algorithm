29ab291c63388904d6a816baf8c28fca88c50256
its_rittik <rittikdas2017@gmail.com>
2024-05-30 15:41:05 +0600

    Space optimised fibonacci also added

--- Dynamic Programing/fibonacci.cpp
+++ Dynamic Programing/fibonacci.cpp
@@ -24,6 +24,7 @@ int dp_fibo(int n , vector<int> &dp){
 
 //dp iteration
 
+/*
 void dp_fibo(int n){
     vector<int> dp(n+1,-1);
 
@@ -33,13 +34,27 @@ void dp_fibo(int n){
 
     cout<<dp[n]<<endl;
 }
+*/
+
+//space optimization
+
+void dp_space_optimised_fibo(int n){
+    int prev1 = 1 , prev2 = 1 , current = 0;
+    for(int i = 2 ; i <= n ; i++){
+        current = prev1+prev2;
+        
+        prev2 = prev1;
+        prev1 = current;
+    }
+    cout<<current<<endl;
+}
 
 int main(){
     int n;
     vector<int> dp(n+1,-1);
     cin>>n;
 
-    dp_fibo(n);
+    dp_space_optimised_fibo(n);
 
     return 0;
 }
